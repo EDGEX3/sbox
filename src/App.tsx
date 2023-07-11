7bb19abb8e19109c0d91172ea255262dbbeca7eb
mrsbytehero <himanshu.creator.info@gmail.com>
2023-07-11 16:52:48 +0530

    install joti and iconoir icon pack 😊

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./css/index.css";
 import Swindow from "./component/Swindow";
 import Loading from "./component/comman/macro/Loading";
 import { LoadingProvider } from "./redux/context/LoadingContext";
-import '@icon-park/react/styles/index.css';
 function App() {
   return (
     <LoadingProvider>
